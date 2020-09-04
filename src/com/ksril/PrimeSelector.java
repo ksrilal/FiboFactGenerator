971b6d226b6c9ba2c1365f96866097aab7e75410
ksrilal <ksrilalranasinghe95@gmail.com>
2020-09-04 10:06:09 +0530

    simplizer prime

--- src/com/ksril/PrimeSelector.java
+++ src/com/ksril/PrimeSelector.java
@@ -3,11 +3,13 @@ package com.ksril;
 public class PrimeSelector {
 
     public int prime(int n) {
-        int m = n/2;
+        //int m = n/2;
         if(n==0 || n==1)
             return 0;
+        else if(n==2)
+            return n;
         else{
-            for(int i=2;i<=m;i++){
+            for(int i=2;i<n;i++){
                 if(n%i==0)
                     return 0;
             }
